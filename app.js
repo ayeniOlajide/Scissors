192c72da5cf9d00df35794395a849d078059e6bb
MACBOOK <macbook@MacBook-Pro.local>
2024-08-15 12:48:29 +0100

    changes in the app.js

--- app.js
+++ app.js
@@ -15,93 +15,91 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require("swagger-jsdoc")
 
 async function initializeExpressServer() {
-    try {
-        const app = express();
+    const app = express();
 
-        module.exports = app;
+    module.exports = app;
 
-        // Connect to MongoDB and initialize Redis
-        await connectDB();
-        await initializeRedisClient();
+    // Connect to MongoDB and initialize Redis
+    await connectDB();
+    await initializeRedisClient();
 
-        // Middleware setup
-        app.use(morgan('dev'));
-        app.use(express.json());
-        app.use(cookieParser());
-        app.use(rateLimiter);
-        app.set('view engine', 'ejs');
-        app.use(express.urlencoded({ extended: false }));
+    // Middleware setup
+    app.use(morgan('dev'));
+    app.use(express.json());
+    app.use(cookieParser());
+    app.use(rateLimiter);
+    app.set('view engine', 'ejs');
+    app.use(express.urlencoded({ extended: false }));
+    
 
-        const PORT = process.env.PORT || 8080;
+    const PORT = process.env.PORT || 8080;
 
-        // Define routes
-        app.use('/auth', authRoutes);
-        app.use('/urls', verifyToken, urlRoutes);
+    // Define routes
+    app.use('/auth', authRoutes);
+    app.use('/urls', verifyToken, urlRoutes);
 
-        const swaggerOptions = {
-            swaggerDefinition: {
-                openapi: '3.0.0',
-                info: {
-                    version: '1.0.0',
-                    title: 'Scissors URL shortener API ',
-                    description: 'API documentation for the URL Shortener service',
-                    contact: {
-                        name: 'Olajide',
-                    },
-                    servers: ['http://localhost:3000']
-                },
-                schemes: ['http', 'https'],
+const swaggerOptions = {
+        swaggerDefinition: {
+          openapi: '3.0.0',
+          info: {
+            version: '1.0.0',
+            title: 'Scissors URL shortener API ',
+            description: 'API documentation for the URL Shortener service',
+            contact: {
+              name: 'Olajide',
             },
-            apis: ['./routes/*.js']
+            servers: ['http://localhost:3000']
+          },
+          schemes: ['http', 'https'],
+        },
+        apis: ['./routes/*.js']
+      }
+      
+      const swaggerDocs = swaggerJsDoc(swaggerOptions)
+      app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+
+    app.get('/', (req, res) => {
+        res.render('index');
+    });
+
+    // Handle short URL redirects
+    app.get('/:shortUrl', redisCachingMiddleware(), async (req, res, next) => {
+        try {
+            const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+            if (!shortUrl) return res.sendStatus(404);
+
+            shortUrl.clicks++;
+            await shortUrl.save();
+
+            res.redirect(shortUrl.full);
+        } catch (err) {
+            next(err);
         }
+    });
 
-        const swaggerDocs = swaggerJsDoc(swaggerOptions)
-        app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
-
-        app.get('/', (req, res) => {
-            res.render('index');
+    // Error handler
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.send({
+            error: {
+                status: err.status || 500,
+                message: err.message,
+            },
         });
+    });
 
-        // Handle short URL redirects
-        app.get('/:shortUrl', redisCachingMiddleware(), async (req, res, next) => {
-            try {
-                const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
-                if (!shortUrl) return res.sendStatus(404);
-
-                shortUrl.clicks++;
-                await shortUrl.save();
-
-                res.redirect(shortUrl.full);
-            } catch (err) {
-                next(err);
-            }
+    // Start the server
+    if (require.main === module) {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
         });
-
-        // Error handler
-        app.use((err, req, res, next) => {
-            res.status(err.status || 500);
-            res.send({
-                error: {
-                    status: err.status || 500,
-                    message: err.message,
-                },
-            });
-        });
-
-        // Start the server
-        if (require.main === module) {
-            app.listen(PORT, () => {
-                console.log(`Server running on port ${PORT}`);
-            });
-        }
-    } catch (e) {
-        console.error('Error during server initialization:', e);
-        process.exit(1);
     }
 }
 
-initializeExpressServer();
-
+initializeExpressServer()
+    .then()
+    .catch((e) => console.error(e));
 
     process.on('SIGINT', async () => {
         try {
